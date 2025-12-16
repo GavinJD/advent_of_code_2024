0cfd550a13ba4c99cc3fbea30b4fd97ef77c6a6c
GavinJD <gavinjd99@gmail.com>
2025-12-16 18:52:31 +0530

    Day 1 :)

--- src/common/root.zig
+++ src/common/root.zig
@@ -1,23 +1,18 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+pub fn read_input(allocator: std.mem.Allocator, day: u8) ![]const u8 {
+    const input_file = try std.fs.cwd().openFile(try std.fmt.allocPrint(allocator, "inputs/day_{}.txt", .{day}), .{ .mode = .read_only });
+    defer input_file.close();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const file_size = try input_file.getEndPos();
+    const buf: []u8 = try allocator.alloc(u8, file_size);
 
-    try stdout.flush(); // Don't forget to flush!
+    _ = try input_file.readAll(buf);
+
+    return buf;
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
+// test "basic add functionality" {
+//     try std.testing.expect(add(3, 7) == 10);
+// }
--- src/day_1.zig
+++ src/day_1.zig
@@ -1,6 +1,52 @@
 const std = @import("std");
 const lib = @import("aoc_2024");
 
-pub fn main() void {
-    std.debug.print("Day {}!", .{lib.add(0, 1)});
+pub fn main() !void {
+    var alloc = std.heap.DebugAllocator(.{}){};
+    // defer _ = alloc.detectLeaks();
+    const allocator = alloc.allocator();
+
+    const input = try lib.read_input(allocator, 1);
+    defer allocator.free(input);
+
+    var left = try std.ArrayList(u32).initCapacity(allocator, 100);
+    defer left.clearAndFree(allocator);
+    var right = try std.ArrayList(u32).initCapacity(allocator, 100);
+    defer right.clearAndFree(allocator);
+
+    var lines = std.mem.splitScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        var nums = std.mem.tokenizeAny(u8, line, " \t\r\n");
+        const left_num = nums.next().?;
+        const right_num = nums.next().?;
+
+        try left.append(allocator, try std.fmt.parseInt(u32, left_num, 10));
+        try right.append(allocator, try std.fmt.parseInt(u32, right_num, 10));
+    }
+
+    std.mem.sort(u32, left.items, {}, std.sort.asc(u32));
+    std.mem.sort(u32, right.items, {}, std.sort.asc(u32));
+
+    var distance: u32 = 0;
+    for (0..left.items.len) |i| {
+        distance += @max(left.items[i], right.items[i]) - @min(left.items[i], right.items[i]);
+    }
+
+    std.debug.print("Part 1: Total distance - {}\n", .{distance});
+
+    var right_counts = std.AutoHashMap(u32, u32).init(allocator);
+    for (right.items) |rn| {
+        if (right_counts.getEntry(rn)) |e| {
+            e.value_ptr.* += 1;
+        } else {
+            try right_counts.put(rn, 1);
+        }
+    }
+
+    var similarity: u32 = 0;
+    for (left.items) |ln| {
+        similarity += ln * (right_counts.get(ln) orelse 0);
+    }
+
+    std.debug.print("Part 2: Similarity score - {}", .{similarity});
 }
