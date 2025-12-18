34b1f113f6efeb6164390ec77f12ebb62ec54cff
GavinJD <gavinjd99@gmail.com>
2025-12-18 12:49:15 +0530

    Fix mem leak

--- src/common/root.zig
+++ src/common/root.zig
@@ -2,7 +2,10 @@
 const std = @import("std");
 
 pub fn read_input(allocator: std.mem.Allocator, day: u8) ![]const u8 {
-    const input_file = try std.fs.cwd().openFile(try std.fmt.allocPrint(allocator, "inputs/day_{}.txt", .{day}), .{ .mode = .read_only });
+    const path = try std.fmt.allocPrint(allocator, "inputs/day_{}.txt", .{day});
+    defer allocator.free(path);
+
+    const input_file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
     defer input_file.close();
 
     const file_size = try input_file.getEndPos();
